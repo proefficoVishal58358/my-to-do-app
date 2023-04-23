113f106e0af21422026af94640d3452cd833d993
VISHAL <proeffico.vishal@gmail.com>
2023-04-24 00:42:56 +0530

    to do app  start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import To_do from "./Components/To_do";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <To_do></To_do>
     </div>
   );
 }
---
+++ src/Components/To_do.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { useState } from "react";
+
+export default function To_do() {
+  const [task, setTask] = useState("");
+
+  const [data, setData] = useState([]);
+
+  const [todo, addTodo] = useState(data);
+  // const [dlt, setDlt] = useState([]);
+
+  const addTask = () => {
+    if (task !== "") {
+      data.push(task);
+      setData([...data]);
+    }
+  };
+  const show = () => {
+    if (data) addTodo([...data]);
+  };
+
+  const dltTask = (index) => {};
+
+  const deleteAll = () => {
+    if (todo.length > 0) {
+      addTodo([]);
+      setData([]);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="form">
+        <input
+          onChange={(e) => setTask(e.target.value)}
+          type="text"
+          className="input"
+          placeholder="Enter your task"
+          value={task}
+          required
+        />
+        <button onClick={addTask} type="button" className="add">
+          Add Task
+        </button>
+        <button onClick={show} type="button" className="add">
+          Show Task
+        </button>
+      </div>
+      {todo?.map((ele, index) => {
+        return (
+          <>
+            <div key={index}>
+              <div className="tasks">{ele} </div>
+            </div>
+          </>
+        );
+      })}
+
+      <div onClick={deleteAll} className="delete-all">
+        Delete all
+      </div>
+    </div>
+  );
+}
